3c6ca8bb444e54f3cb7c8e6d378f719a85a510a6
csunijena <christian.stolze@uni-jena.de>
2026-06-23 13:31:32 +0200

    fix: rely on static BeBlob script, remove dynamic injection

--- src/components/scripts/comments.inline.ts
+++ src/components/scripts/comments.inline.ts
@@ -127,25 +127,6 @@ if (typeof document !== "undefined") {
       document.addEventListener("themechange", themeChangeHandler);
       addCleanup(() => document.removeEventListener("themechange", themeChangeHandler));
     } else if (beblobContainer) {
-      if (!document.getElementById("beblob-script")) {
-        const beblobScript = document.createElement("script");
-        beblobScript.id = "beblob-script";
-        beblobScript.src = "https://unpkg.com/beblob@2.1.0/dist/beblob.js";
-        beblobScript.setAttribute("data-client-id", beblobContainer.dataset.clientId);
-        beblobScript.setAttribute("data-redirect-uri", beblobContainer.dataset.redirectUri);
-        beblobScript.setAttribute("data-project-name", beblobContainer.dataset.projectName);
-        beblobScript.setAttribute(
-          "data-issue-mapping-strategy",
-          beblobContainer.dataset.issueMappingStrategy,
-        );
-        beblobScript.setAttribute("data-theme", beblobContainer.dataset.theme);
-        beblobScript.setAttribute("data-lang", beblobContainer.dataset.lang);
-        beblobScript.setAttribute("data-gitlab-url", beblobContainer.dataset.gitlabUrl);
-        beblobScript.setAttribute("data-dev-mode", "true");
-
-        beblobContainer.appendChild(beblobScript);
-      }
-
       const themeChangeHandler = changeBeBlobTheme;
       document.addEventListener("themechange", themeChangeHandler);
       addCleanup(() => document.removeEventListener("themechange", themeChangeHandler));
